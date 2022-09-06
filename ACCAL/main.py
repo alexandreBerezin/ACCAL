d554b5dda27dbd2a891b4b61e4323d496b229d04
Alexandre <alexandre.berezin@student.isae-supaero.fr>
2022-09-06 11:05:28 +0200

    Branch to implement Multiprocessing

--- ACCAL/main.py
+++ ACCAL/main.py
@@ -32,23 +32,25 @@ PROB_LIMIT = 0.5
 ###################################################
 ###################################################
 
+#Close for multiprocessing 
+if __name__ == "__main__":
 
-### Compute and save tomporary images used for further processing
-#modules.imageProcessing.computeAndSaveTempImages(dataFolderPath=dataFolderPath,denoiseRatio=DENOISE_RATIO,clipLimit=CLIP_LIMIT)
-
-
-### Compute and save features for each image 
-#functions.saveFeatures(dataFolderPath=dataFolderPath,absAppPath=appPath,pixelSide=PIXEL_SIDE,lengthKernel=LENGTH_KERNEL,featureNumber=FEATURE_NUMBER)
+    ### Compute and save tomporary images used for further processing
+    #modules.imageProcessing.computeAndSaveTempImages(dataFolderPath=dataFolderPath,denoiseRatio=DENOISE_RATIO,clipLimit=CLIP_LIMIT)
 
 
-## Compute and save the dissimilarity Matrix
-#functions.saveDissMatrix(dataFolderPath=dataFolderPath)
+    ### Compute and save features for each image 
+    #functions.saveFeatures(dataFolderPath=dataFolderPath,absAppPath=appPath,pixelSide=PIXEL_SIDE,lengthKernel=LENGTH_KERNEL,featureNumber=FEATURE_NUMBER)
 
 
-## Compute and save Probability matrix
-#functions.clustering(dataFolderPath=dataFolderPath,distCut=DIST_CUT,nbBurnin=NB_BURNIN,nbSample=NB_SAMPLE,eachSample=EACH_SAMPLE)
+    ## Compute and save the dissimilarity Matrix
+    #functions.saveDissMatrix(dataFolderPath=dataFolderPath)
 
 
-## Save Results
-visualisation.saveResults(dataFolderPath=dataFolderPath,probLimit=PROB_LIMIT)
+    ## Compute and save Probability matrix
+    #functions.clustering(dataFolderPath=dataFolderPath,distCut=DIST_CUT,nbBurnin=NB_BURNIN,nbSample=NB_SAMPLE,eachSample=EACH_SAMPLE)
+
+
+    ## Save Results
+    visualisation.saveResults(dataFolderPath=dataFolderPath,probLimit=PROB_LIMIT)
 
